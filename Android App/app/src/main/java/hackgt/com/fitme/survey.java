169e5b43fd99085448c98260b2430ddca85606fc
Siddarth Senthilkumar <ssenthilkumar3@gatech.edu>
2015-09-26 22:10:58 -0400

    Linked screens

--- Android App/app/src/main/java/hackgt/com/fitme/survey.java
+++ Android App/app/src/main/java/hackgt/com/fitme/survey.java
@@ -1,9 +1,11 @@
 package hackgt.com.fitme;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class Survey extends Activity {
 
@@ -33,5 +35,10 @@ public class Survey extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void selectTrainer(View view) {
+        Intent intent = new Intent(this, Trainerlist.class);
+        startActivity(intent);
     }
 }
